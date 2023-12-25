623375a9651a86f1511992938bc35386b3cce2a7
divopsor <divopsor@gmail.com>
2023-12-25 19:01:56 +0900

    feat: 기본 페이지 추가 (8) [skip vercel]

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,15 +1,11 @@
 import { DetailsPage } from "../containers/DetailsPage";
 
-export default function ({ params }: { params: { id: string }}) {
+export default function ({ params }: { params: { id: string; body: { contents: string; createdAt: number } }}) {
   return <DetailsPage params={params} />;
 }
 
 export async function generateStaticParams() {
   const { data: posts } = await fetch('https://blog.creco.dev/api/gist/ce62b77189108398c8655c33dbb608ee/list').then((res) => res.json())
- 
-  console.log({ items: posts.items });
 
-  return posts.items.map((post: any) => ({
-    id: post.id,
-  }))
+  return posts.items.map((post: any) => post);
 }
--- app/containers/DetailsPage.tsx
+++ app/containers/DetailsPage.tsx
@@ -5,17 +5,26 @@ import { API } from '../api/index';
 import { BLOG_POST } from '../api/constant';
 import { Card } from '@divops-packages/ui';
 
-export const DetailsPage = ({ params }: { params: { id: string }}) => {
+export const DetailsPage = ({ params }: { params: { id: string; body?: { contents: string; createdAt: number } }}) => {
+  console.log({ params });
+
   const category = BLOG_POST;
   const { data } = useQuery(
-    ["API.of().readItem", category, params.id],
-    () => API.of(category).readItem(params.id as string)
+    ["API.of().readItem", category, params.id, params.body],
+    async () => {
+      if (params.body == null) {
+        return await API.of(category).readItem(params.id as string)
+      }
+
+      return params;
+    }
   );
 
   const [title, ...body] = (data ?? {})?.data?.body.contents.trim().split('\n') ?? [];
 
   return (
     <div>
+      <p style={{ fontSize: '8px' }}>{params?.id}</p>
       <h2>{title}</h2>
       <Card>{body}</Card>
     </div>
