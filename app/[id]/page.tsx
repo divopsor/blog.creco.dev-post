bb4d14e816cd95fb85e80b45cbab9e9dcf675d30
divopsor <divopsor@gmail.com>
2023-12-25 23:35:48 +0900

    feat(post): generateStaticParams generateMetadata 추가 [skip vercel]

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,11 +1,34 @@
 import { DetailsPage } from "../containers/DetailsPage";
 
+let cache: any;
+async function fetchList () {
+  if (cache != null) {
+    return cache;
+  }
+
+  const { data: posts } = await fetch('https://blog.creco.dev/api/gist/ce62b77189108398c8655c33dbb608ee/list').then((res) => res.json())
+  
+  cache = posts;
+  
+  return cache;
+}
+
 export default function ({ params }: { params: { id: string; body: { contents: string; createdAt: number } }}) {
   return <DetailsPage params={params} />;
 }
 
-export async function generateStaticParams() {
-  const { data: posts } = await fetch('https://blog.creco.dev/api/gist/ce62b77189108398c8655c33dbb608ee/list').then((res) => res.json())
+export const generateStaticParams = async () => {
+  const posts = await fetchList();
+  const result = posts.items.map((post: any) => {
+    return { id: post.id };
+  });
 
-  return posts.items.map((post: any) => post);
+  return result;
+}
+
+export const generateMetadata = async ({ params }: any) => {
+  const { id } = params;
+  const posts = await fetchList();
+
+  return posts.items.find((post: any) => post.id === id)!;
 }
