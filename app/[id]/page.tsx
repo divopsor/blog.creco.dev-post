ee53026b3fa636b73ebba15998ecd5b15a72ff9e
divopsor <divopsor@gmail.com>
2023-12-25 18:43:20 +0900

    feat: 기본 페이지 추가 (5) [skip vercel]

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,1 +1,15 @@
-export { DetailsPage as default, generateStaticParams } from "../containers/DetailsPage";
+import { DetailsPage } from "../containers/DetailsPage";
+
+export default function ({ params }: { params: { id: string }}) {
+  return <DetailsPage params={params} />;
+}
+
+export async function generateStaticParams() {
+  const { data: posts } = await fetch('https://blog.creco.dev/api/gist/ce62b77189108398c8655c33dbb608ee/list').then((res) => res.json())
+ 
+  console.log({ posts });
+
+  return posts.map((post: any) => ({
+    id: post.id,
+  }))
+}
--- app/containers/DetailsPage.tsx
+++ app/containers/DetailsPage.tsx
@@ -21,13 +21,3 @@ export const DetailsPage = ({ params }: { params: { id: string }}) => {
     </div>
   )
 };
-
-export async function generateStaticParams() {
-  const { data: posts } = await fetch('https://blog.creco.dev/api/gist/ce62b77189108398c8655c33dbb608ee/list').then((res) => res.json())
- 
-  console.log({ posts });
-
-  return posts.map((post: any) => ({
-    id: post.id,
-  }))
-}
