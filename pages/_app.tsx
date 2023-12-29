cafaf24620e428c65a5c700142d202e122febd7f
divopsor <divopsor@gmail.com>
2023-12-30 00:27:35 +0900

    feat: GlobalCss [skip vercel]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,9 @@ import { QueryClient } from '@tanstack/react-query';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CrecoApp, Colors } from '@divops-packages/blog-creco-dev';
-import '@divops-packages/blog-creco-dev/crecoApp.css';
-import '@divops-packages/blog-creco-dev/global.css';
 
 const queryClient = new QueryClient();
-
+const cssText = CrecoApp.GlobalCss;
 export default function App({ Component, pageProps }: AppProps) {
   pageProps.style = {
     ...pageProps.style,
@@ -21,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <CrecoApp queryClient={queryClient}>
         <Component {...pageProps} />
       </CrecoApp>
+      <style global jsx>{cssText}</style>
     </>
   )
 }
