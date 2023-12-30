ebccf1e10644cd78b90ad7ed5f30866037645735
divopsor <divopsor@gmail.com>
2023-12-30 19:21:32 +0900

    test: 404 page 4

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,7 @@ export function parsePost(post?: any) {
   const details = post;
 
   const [category, title, ...body] = details?.body?.contents?.trim?.().split?.('\n') ?? [];
-  const timestamp = details?.body.updatedAt ?? details?.body?.createdAt;
+  const timestamp = details?.body?.updatedAt ?? details?.body?.createdAt;
   const date = new Date(timestamp);
 
   return {
