087464195acdf3a43c648d9cc2880e2dafbd3238
divopsor <divopsor@gmail.com>
2023-12-30 19:19:25 +0900

    test: 404 page 3

--- src/containers/NotFoundPage.tsx
+++ src/containers/NotFoundPage.tsx
@@ -1,6 +1,16 @@
 import { usePathname } from 'next/navigation'
+import { DetailsPage } from './DetailsPage';
 
 export default function NotFoundPage() {
   const pathName = usePathname();
+  const [, id] = pathName.split('/');
+
+  console.log(`pathName.split('/').length`, pathName.split('/').length);
+  console.log(`pathName.startsWith('/')`, pathName.startsWith('/'));
+
+  if (pathName.split('/').length === 2 && pathName.startsWith('/')) {
+    return <DetailsPage post={{ id }}/>
+  }
+
   return <>404 page: {pathName}</>
 }
