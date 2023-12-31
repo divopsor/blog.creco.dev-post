60cbd0bdfeb6fca57c33ccea2906e8a75a14957a
divopsor <divopsor@gmail.com>
2023-12-31 10:54:04 +0900

    update 2

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,11 +1,16 @@
 import { getCrecoAppStaticPaths, getCrecoAppStaticProps } from '@divops-packages/blog-creco-dev';
 import { GetStaticProps } from 'next';
 import { DetailsPage } from '../../src/containers/DetailsPage';
+import NotFoundPage from '../../src/containers/NotFoundPage';
 
 export const getStaticPaths = async () => getCrecoAppStaticPaths({ category: 'blog-post' });
 
 export const getStaticProps: GetStaticProps = async (context) => getCrecoAppStaticProps(context, { category: 'blog-post' });
 
-export default function ({ post }: { post: { id: string; body?: { contents: string; createdAt: number; updatedAt: number; }}}) {
+export default function ({ post }: { post?: { id: string; body?: { contents: string; createdAt: number; updatedAt: number; }}}) {
+  if (post == null) {
+    return <NotFoundPage />;
+  }
+
   return <DetailsPage post={post} />;
 }
--- src/containers/NotFoundPage.tsx
+++ src/containers/NotFoundPage.tsx
@@ -3,12 +3,9 @@ import { DetailsPage } from './DetailsPage';
 
 export default function NotFoundPage() {
   const pathName = usePathname();
-  const [, id] = pathName.split('/');
+  const [, id] = pathName?.split('/') ?? [];
 
-  console.log(`pathName.split('/').length`, pathName.split('/').length);
-  console.log(`pathName.startsWith('/')`, pathName.startsWith('/'));
-
-  if (pathName.split('/').filter(x => x !== '').length === 1 && pathName.startsWith('/')) {
+  if (pathName?.split('/').filter(x => x !== '').length === 1 && pathName.startsWith('/')) {
     return <DetailsPage post={{ id }}/>
   }
 
