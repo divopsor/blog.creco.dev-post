a891ff46593c3ea67b6e0bb29daff3579008c515
divopsor <divopsor@gmail.com>
2023-12-31 10:59:19 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { CATEGORY } from '../src/constants';
+
 export { HomePage as default } from '../src/containers/HomePage';
 
 export const getStaticProps = async () => {
-  const url = `https://blog.creco.dev/api/gist/blog-post/list`;
+  const url = `https://blog.creco.dev/api/gist/${CATEGORY}/list`;
 
   const { data } = await fetch(url).then((res) => res.json());
 
