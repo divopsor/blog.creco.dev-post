974adddc3621cd61377dc7ea839fa055b424d3f5
divopsor <divopsor@gmail.com>
2024-09-01 02:14:42 +0900

    update

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,14 +1,32 @@
-import { getCrecoAppStaticPaths, getCrecoAppStaticProps } from '@divops-packages/blog-creco-dev';
-import { GetStaticProps } from 'next';
-import { CATEGORY } from '../../src/constants';
-import { DetailsPage } from '../../src/containers/DetailsPage';
-import NotFoundPage from '../../src/containers/NotFoundPage';
+import {
+  getCrecoAppStaticPaths,
+  getCrecoAppStaticProps,
+} from "@divops-packages/blog-creco-dev";
+import { GetStaticProps } from "next";
+import { CATEGORY } from "../../src/constants";
+import { DetailsPage } from "../../src/containers/DetailsPage";
+import NotFoundPage from "../../src/containers/NotFoundPage";
 
-export const getStaticPaths = async () => getCrecoAppStaticPaths({ category: CATEGORY });
+export const getStaticPaths = async () =>
+  getCrecoAppStaticPaths({
+    category: CATEGORY,
+    prefix: "/github-api/gist",
+  });
 
-export const getStaticProps: GetStaticProps = async (context) => getCrecoAppStaticProps(context, { category: CATEGORY });
+export const getStaticProps: GetStaticProps = async (context) =>
+  getCrecoAppStaticProps(context, {
+    category: CATEGORY,
+    prefix: "/github-api/gist",
+  });
 
-export default function ({ item }: { item?: { id: string; body?: { contents: string; createdAt: number; updatedAt: number; }}}) {
+export default function ({
+  item,
+}: {
+  item?: {
+    id: string;
+    body?: { contents: string; createdAt: number; updatedAt: number };
+  };
+}) {
   if (item == null) {
     return <NotFoundPage />;
   }
