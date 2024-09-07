790afd7afe191c66f189bfbed43f676caf0fc989
divopsor <divopsor@gmail.com>
2024-09-07 17:03:30 +0900

    update homepage

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -24,55 +24,60 @@ export const HomePage = ({ list: initialList }: { list: any }) => {
       mainBackgroundColor={Colors.Dark}
       subBackgroundColor={Colors.DeepDark}
       fontColor={Colors.SoftWhite}
-      desktopPageWidth="840px"
+      desktopPageWidth="1200px"
     >
-      <Header>
-        <Logo />
-      </Header>
+      <div style={{
+        maxWidth: '840px',
+        margin: "0 auto",
+      }}>
+        <Header>
+          <Logo />
+        </Header>
 
-      <Spacing size={20} />
+        <Spacing size={20} />
 
-      <p style={{ fontSize: '60px', fontWeight: 'bold' }}>Posts</p>
+        <p style={{ fontSize: '60px', fontWeight: 'bold' }}>Posts</p>
 
-      <Spacing size={14} />
+        <Spacing size={14} />
 
-      <p style={{ fontSize: '20px', color: 'grey', fontWeight: 'light' }}>Creco</p>
+        <p style={{ fontSize: '20px', color: 'grey', fontWeight: 'light' }}>Creco</p>
 
-      <Spacing size={20} />
+        <Spacing size={20} />
 
-      <Divider />
+        <Divider />
 
-      <ul>
-      {
-        list.map((item: any) => {
-          return (
-            <li key={item.id} style={{
-              width: '100%',
-              borderBottom: '1px solid rgba(229, 231, 235, 0.5)',
-              padding: '48px 0',
-            }}>
-              <Link
-                href={`/${item.id}`}
-                className="clickable"
-                style={{
-                  fontSize: '1.6rem',
-                  textDecoration: 'unset',
-                }}
-              >
-                <time style={{ color: 'grey' }}>{formattedDateYYYYMMDD(new Date(item.createdAt))}</time> <br />
-                <Spacing size={4} />
-                <span style={{ lineHeight: '100%', fontSize: '1.2rem' }}>{item.category}</span>
-                <h3>{item.title}</h3>
-                <Spacing size={20} />
-                <span style={{ lineHeight: '100%', color: Colors.SoftGrey }}>{item.body.split(/<br>|\n/)[0]}</span>
-                <Spacing size={20} />
-                <p style={{ color: 'rgb(14, 165, 233)' }}>Read more →</p>
-              </Link>
-            </li>
-          );
-        })
-      }
-      </ul>
+        <ul>
+        {
+          list.map((item: any) => {
+            return (
+              <li key={item.id} style={{
+                width: '100%',
+                borderBottom: '1px solid rgba(229, 231, 235, 0.5)',
+                padding: '48px 0',
+              }}>
+                <Link
+                  href={`/${item.id}`}
+                  className="clickable"
+                  style={{
+                    fontSize: '1.6rem',
+                    textDecoration: 'unset',
+                  }}
+                >
+                  <time style={{ color: 'grey' }}>{formattedDateYYYYMMDD(new Date(item.createdAt))}</time> <br />
+                  <Spacing size={4} />
+                  <span style={{ lineHeight: '100%', fontSize: '1.2rem' }}>{item.category}</span>
+                  <h3>{item.title}</h3>
+                  <Spacing size={20} />
+                  <span style={{ lineHeight: '100%', color: Colors.SoftGrey }}>{item.body.split(/<br>|\n/)[0]}</span>
+                  <Spacing size={20} />
+                  <p style={{ color: 'rgb(14, 165, 233)' }}>Read more →</p>
+                </Link>
+              </li>
+            );
+          })
+        }
+        </ul>
+      </div>
     </ResponsivePage>
   )
 };
